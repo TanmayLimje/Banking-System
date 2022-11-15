c2f4cc55b6b33fd4ba564c588b1000ff021bf0cd
Tanmay Limje <103094622+TanmayLimje@users.noreply.github.com>
2022-11-16 00:02:03 +0530

    Update Main.java
    Final

--- Main.java
+++ Main.java
@@ -1,17 +1,14 @@
-import java.util.*;
-
 public class Main
 {
     public static void main(String args[])
     {
-        Account account = new Account("Anya","1234567890",19,15,2003,9,1234567890,"123456789012",1000);
-        Bank_database bd= new Bank_database(account);
-        bd.withdraw(20);
-        bd.get_info();
-        bd.deposit(20);
-        bd.get_info();
-        bd.show_tactions();
 
+        Login login = new Login();
+        Account_Info account = null;
+
+        while (account == null)
+            account = login.loginInitiator();
+        login.Bank_processor(account);
 
 
     }
